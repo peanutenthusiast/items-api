d559ae02d09845c1b2dd2c3aa13f54b825dd489f
Mark de Dios <mhdedios@gmail.com>
2022-12-21 01:19:25 -0800

    Add create function for items service

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from 'src/interfaces/item.interface';
+import { v4 } from 'uuid';
+@Injectable('items')
+export class ItemsService {
+    private readonly items: Item[] = [];
+    private readonly Set<string> itemNames = new this.Set();
 
-@Injectable()
-export class ItemsService {}
+    create(item: { name: string }) {
+        if (this.itemNames.has(item.name)) throw new Error('cannot add duplicate item name');
+        if (!item.name.length) throw new Error('cannot add item with empty item name')
+        this.items.push({...item, id: v4() });
+    }
+}
