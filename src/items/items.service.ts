0d1e488ebda6937e4314bb6ab00eac2b0038a424
Mark de Dios <mhdedios@gmail.com>
2022-12-21 01:26:18 -0800

    Add create function to items service which creates a unique item and validates a item name with length more than 0

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from 'src/interfaces/item.interface';
+import { v4 } from 'uuid'
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+    private readonly items: Item[] = [];
+    private readonly itemNames: Set<string> = new Set();
+
+    create(item: Pick<Item, "name">) {
+        if (this.itemNames.has(item.name)) throw new Error('cannot item with duplicate name');
+        if (!item.name.length) throw new Error('cannot add item with empty name');
+
+        this.items.push({...item, id: v4()})
+        this.itemNames.add(item.name)
+    }
+}
