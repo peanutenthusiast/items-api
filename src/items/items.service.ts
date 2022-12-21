dc0656b51a00634e4ae1812ece2f8cffdbaf8fb4
Mark de Dios <mhdedios@gmail.com>
2022-12-21 01:20:25 -0800

    Revert "Add create function for items service"
    This reverts commit d559ae02d09845c1b2dd2c3aa13f54b825dd489f.

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,14 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { Item } from 'src/interfaces/item.interface';
-import { v4 } from 'uuid';
-@Injectable('items')
-export class ItemsService {
-    private readonly items: Item[] = [];
-    private readonly Set<string> itemNames = new this.Set();
 
-    create(item: { name: string }) {
-        if (this.itemNames.has(item.name)) throw new Error('cannot add duplicate item name');
-        if (!item.name.length) throw new Error('cannot add item with empty item name')
-        this.items.push({...item, id: v4() });
-    }
-}
+@Injectable()
+export class ItemsService {}
